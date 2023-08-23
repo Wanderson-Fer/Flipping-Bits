f2277bcbfb3926da49709f7a5307e83cf41e3c09
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-08-23 09:40:26 -0400

    flipper added

---
+++ flipper.py
@@ -0,0 +1,39 @@
+def convert_to_bits(n):
+    converted_n = ''
+
+    for e in range(31, -1, -1):
+        if n - 2 ** e >= 0:
+            converted_n += '1'
+        else:
+            converted_n += '0'
+
+    return converted_n
+
+
+def convert_from_bits(b):
+    converted_b = 0
+    for e in range(1, 33):
+        if b[-e] == '1':
+            converted_b += 2 ** (e - 1)
+
+    return converted_b
+
+
+def flipping_bits(n):
+    # Write your code here
+    n_in_bits = convert_to_bits(n)
+
+    n_bits_flipped = ''
+    for bit in n_in_bits:
+        if bit == '0':
+            n_bits_flipped += '1'
+        else:
+            n_bits_flipped += '0'
+
+    n_int_flipped = convert_from_bits(n_bits_flipped)
+
+    return n_int_flipped
+
+
+if __name__ == '__main__':
+    print('hi')
